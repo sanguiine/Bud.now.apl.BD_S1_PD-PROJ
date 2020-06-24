afa3c47aee9d6b4814ad4ba2243a1630ed1ebc83
DZIOWANI98 <derbinmichal11@gmail.com>
2020-06-24 09:49:23 +0200

    files removed

--- src/main/java/com/example/UrbanLibrary/dao/entity/repositories/AuthorsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.UrbanLibrary.dao.entity.repositories;
-
-import com.example.UrbanLibrary.dao.entity.Authors;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorsRepository extends CrudRepository<Authors, Long> {
-}
--- src/main/java/com/example/UrbanLibrary/dao/entity/repositories/LoansRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.UrbanLibrary.dao.entity.repositories;
-
-import com.example.UrbanLibrary.dao.entity.Loans;
-import org.springframework.data.repository.CrudRepository;
-
-public interface LoansRepository extends CrudRepository<Loans, Long> {
-}
