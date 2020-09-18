5d28411ab06adee0a6d8b5d2fbb01d39bd6b64ad
DZIOWANI98 <derbinmichal11@gmail.com>
2020-09-18 18:45:42 +0200

    update

--- UrbanLibrary/src/main/java/com/example/UrbanLibrary/dao/entity/repositories/MembersRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.UrbanLibrary.dao.entity.repositories;
-
-import com.example.UrbanLibrary.dao.entity.Members;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MembersRepository extends CrudRepository<Members, Long> {
-
-}
--- UrbanLibrary/src/main/java/com/example/UrbanLibrary/dao/entity/repositories/ReadByMemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.UrbanLibrary.dao.entity.repositories;
-
-import com.example.UrbanLibrary.dao.entity.ReadByMember;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ReadByMemberRepository extends CrudRepository<ReadByMember, Long> {
-
-}
