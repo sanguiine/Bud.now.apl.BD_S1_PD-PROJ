dcd334d9937e16a153461a6d8a472b7c99e0b477
Monika <215858@edu.p.lodz.pl>
2020-09-20 16:10:23 +0200

    statistics for admin

--- UrbanLibrary-ng-app/src/app/components/site/admin/admin.component.ts
+++ UrbanLibrary-ng-app/src/app/components/site/admin/admin.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiService} from "../../../shared/api.service";
+import {Member} from "../../../model/member";
+import {Loan} from "../../../model/loan";
+import {Reservation} from "../../../model/reservation";
+import {Book} from "../../../model/book";
 
 @Component({
   selector: 'app-admin',
@@ -10,10 +15,61 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class AdminComponent implements OnInit {
+  members: Member[] = [];
+  loans: Loan[] = [];
+  reservations: Reservation[] = [];
+  books: Book[] = [];
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
+    this.getAllMembers();
+    this.getAllLoans();
+    this.getAllReservations();
+    this.getAllBooks();
   }
 
+  public getAllMembers(){
+    this.apiService.getAllMembers().subscribe(
+      res => {
+        this.members = res;
+      },
+      err => {
+        alert('An error has occured');
+      }
+    );
+  }
+
+  public getAllLoans(){
+    this.apiService.getAllLoans().subscribe(
+      res => {
+        this.loans = res;
+      },
+      err => {
+        alert('An error has occured');
+      }
+    );
+  }
+
+  public getAllReservations(){
+    this.apiService.getAllReservations().subscribe(
+      res => {
+        this.reservations = res;
+      },
+      err => {
+        alert('An error has occured');
+      }
+    );
+  }
+
+  public getAllBooks(){
+    this.apiService.getAllBooks().subscribe(
+      res => {
+        this.books = res;
+      },
+      err => {
+        alert('An error has occured');
+      }
+    );
+  }
 }
