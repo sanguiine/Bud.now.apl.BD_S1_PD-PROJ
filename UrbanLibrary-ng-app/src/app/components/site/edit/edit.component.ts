5d61d958d96285843bd69f8ea7abee4f8826f968
Monika <215858@edu.p.lodz.pl>
2020-09-20 17:20:25 +0200

    edit for user added

--- UrbanLibrary-ng-app/src/app/components/site/edit/edit.component.ts
+++ UrbanLibrary-ng-app/src/app/components/site/edit/edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Member} from "../../../model/member";
+import {ApiService} from "../../../shared/api.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-edit',
@@ -6,10 +9,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
+  member = JSON.parse(localStorage.getItem("loggedMember"))
 
-  constructor() { }
+  editFirstName = this.member.firstNname;
+  editLastName = this.member.lastName;
+  editEmail = this.member.email;
+  editAddress = this.member.address;
+  editCity = this.member.city;
+  editZIP = this.member.zipCode;
+  editPhone = this.member.phoneNumber;
+  editDate = this.member.birthDate;
+  newPassword = null;
+
+  constructor(private router: Router, private apiService: ApiService) { }
 
   ngOnInit(): void {
   }
 
+  editMember(): void {
+    this.member.email = this.editEmail;
+    this.member.firstNname = this.editFirstName;
+    this.member.lastName = this.editLastName;
+    this.member.address = this.editAddress;
+    this.member.city = this.editCity;
+    this.member.zipCode = this.editZIP;
+    this.member.birthDate = this.editDate;
+    this.member.phoneNumber = this.editPhone;
+
+    if(this.newPassword != null) {
+      this.member.password = this.newPassword;
+    }
+
+    this.apiService.postMember(this.member).subscribe(
+      res => {
+        localStorage.setItem("loggedMember", JSON.stringify(this.member));
+        location.reload();
+      },
+      err => {
+        alert('An error has occured while sending data.');
+      }
+    );
+  }
 }
