87cf44d620b2b1a59e91af2b770c4427653665fd
Monika <215858@edu.p.lodz.pl>
2020-09-20 16:31:32 +0200

    registration added

--- UrbanLibrary-ng-app/src/app/components/site/register/register.component.ts
+++ UrbanLibrary-ng-app/src/app/components/site/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Member} from "../../../model/member";
+import {ApiService} from "../../../shared/api.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  model: Member = {
+    memberID: null,
+    firstNname: '',
+    lastName: '',
+    birthDate: new Date(2000, 1, 1),
+    city: '',
+    zipCode: '',
+    address: '',
+    phoneNumber: '',
+    email: '',
+    password: '',
+    role: 'user',
+    bookReadByMember: null,
+    bookReserved: null,
+    loanlist: null
+  };
 
-  constructor() { }
+  constructor(private router: Router, private apiService: ApiService) { }
 
   ngOnInit(): void {
   }
 
+  addMember(): void {
+    this.apiService.postMember(this.model).subscribe(
+      res => {
+        this.router.navigate(["success"])
+      },
+      err => {
+        alert('An error has occured while sending data.');
+      }
+    );
+  }
 }
