7b3fee5dc9bcc9e5508a0f4c8b0d03fc7b46828f
DZIOWANI98 <derbinmichal11@gmail.com>
2020-09-20 12:19:58 +0200

    update

--- UrbanLibrary/src/main/java/com/example/UrbanLibrary/dao/entity/repositories/BooksRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.UrbanLibrary.dao.entity.repositories;
-
-import com.example.UrbanLibrary.dao.entity.Books;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BooksRepository extends CrudRepository<Books, Long> {
-}
--- UrbanLibrary/src/main/java/com/example/UrbanLibrary/dao/entity/repositories/CategoriesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.UrbanLibrary.dao.entity.repositories;
-
-import com.example.UrbanLibrary.dao.entity.Categories;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoriesRepository extends CrudRepository<Categories, Long> {
-}
